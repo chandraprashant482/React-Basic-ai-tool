098c594c5cd1416b82f253aeb08926b253166086
Prashant <101173058+chandraprashant482@users.noreply.github.com>
2025-12-01 19:22:52 +0530

    Update constants.js

--- src/files/constants.js
+++ src/files/constants.js
@@ -1,1 +1,1 @@
-export const url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyC2KtdmE3L0jcNH_M5_n9wW6mWBjljsh_w"
+export const url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=WRITE_YOURS"
